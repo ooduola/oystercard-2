aedffc5f70de8405e2938136ea26fcdd2172dde8
Olatunji Oduola <ola.oduola@gmail.com>
2020-05-18 15:29:31 +0100

    Added error comments to file

--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -1,3 +1,16 @@
 describe Oystercard do
 
 end
+
+=begin
+An error occurred while loading ./spec/oyster_spec.rb.
+Failure/Error:
+  describe Oystercard do
+  
+  end
+
+NameError:
+  uninitialized constant Oystercard
+# ./spec/oyster_spec.rb:1:in `<top (required)>'
+No examples found.
+=end
